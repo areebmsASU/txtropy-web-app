d942ff05f0584366ae22fb0cee061b55c2496c9d
Areeb Siddiqi <areeb@dropcommerce.com>
2024-03-15 13:15:58 -0400

    Fix Book Text + CSS

--- pages/books/[bookId]/index.js
+++ pages/books/[bookId]/index.js
@@ -1,23 +1,60 @@
-import { useState } from 'react'; 
-
+import { useRouter } from 'next/router';
+import { useState, createElement } from 'react'; 
 import Drawer from './_components/Drawer';
 import TextWindow from './_components/TextWindow';
 import Container from '@mui/material/Container';
 
-export async function getServerSideProps(context) {
-    const bookId = context.query.bookId;
-    const res = await fetch(`https://api.txtropy.com/books/${bookId}`)
-    const initialLines = await res.json()
-    return { props: { initialLines } }
+import Head from 'next/head';
+
+
+function mapToElements(htmlmap){
+  
+  if (htmlmap.attrs && "style" in htmlmap.attrs){
+    const { style, ...attrs } = htmlmap.attrs;
+    attrs["css"] = style;
+    htmlmap.attrs = attrs;
   }
+
+  if (htmlmap.contents) {
+    const contents = htmlmap.contents.map(elem => { 
+      if (typeof elem == "string") {
+        return elem;
+      } else {
+        return mapToElements(elem);
+      }
+    })
+
+    htmlmap.tag = htmlmap.tag == "body" ? "div" : htmlmap.tag
+
+    const newElem = createElement(htmlmap.tag, htmlmap.attrs, ...contents)
+    return newElem;
+  } else {
+    const newElem = createElement(htmlmap.tag, htmlmap.attrs)
+    return newElem;
+  } 
+}
    
 
-function Page({ initialLines }) {
-  const [lines, setLines] = useState(initialLines);
+export async function getServerSideProps(context) {
+    const bookId = context.query.bookId;
+    const res = await fetch(`https://api.txtropy.com/books/${bookId}`);
+    const htmlmap = await res.json();
+    
+    return { props: { htmlmap } }
+  }
+
+function Page({ htmlmap }) {
+  const router = useRouter()
+  const bookId = router.query.bookId;
+
+  //         <Drawer />     
+  // console.log(htmlmap);
   return (
-      <Container >
-        <TextWindow lines={lines} setLines={setLines} />
-        <Drawer chunkIds={lines.map(line => Math.floor(line.id/3)).filter((x, i, a) => a.indexOf(x) === i)}/>
+    <Container >
+      <Head>
+        <link rel="stylesheet" href={`https://api.txtropy.com/books/css/${bookId}`} type="text/css"/>
+      </Head>
+        {mapToElements(htmlmap)}
     </Container>
 
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -18,20 +15,15 @@ export async function getServerSideProps() {
   const res = await fetch('https://api.txtropy.com/books')
   const jres = await res.json()
   const books = await JSON.parse(jres.body)
-  // console.log(books);
-  // Pass data to the page via props
   return { props: { books } }
 }
 
 
 export default function Page({ books }) {
-  console.log(books);
   return (
     <React.Fragment>
       <CssBaseline />
-
       <main className={styles.layout}>
-
         <Box sx={{ paddingTop: 8 }}>
           <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
             Relative Entropy
