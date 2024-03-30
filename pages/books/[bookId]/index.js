15d508a766d0e33d35444f8221a8a77f1b80e082
Areeb Siddiqi <areebms@gmail.com>
2024-03-30 07:50:47 -0400

    Index page fixed. GetStaticProps added to Book pages.

--- pages/books/[bookId]/index.js
+++ pages/books/[bookId]/index.js
@@ -1,62 +1,71 @@
-import { useRouter } from 'next/router';
-import { useState, createElement } from 'react'; 
-import Drawer from './_components/Drawer';
-import TextWindow from './_components/TextWindow';
-import Container from '@mui/material/Container';
+import { useRouter } from "next/router";
+import { useState, createElement } from "react";
+import Drawer from "./_components/Drawer";
+import TextWindow from "./_components/TextWindow";
+import Container from "@mui/material/Container";
 
-import Head from 'next/head';
+import Head from "next/head";
 
-
-function mapToElements(htmlmap){
-  
-  if (htmlmap.attrs && "style" in htmlmap.attrs){
+function mapToElements(htmlmap) {
+  if (htmlmap.attrs && "style" in htmlmap.attrs) {
     const { style, ...attrs } = htmlmap.attrs;
     attrs["css"] = style;
     htmlmap.attrs = attrs;
   }
 
   if (htmlmap.contents) {
-    const contents = htmlmap.contents.map(elem => { 
+    const contents = htmlmap.contents.map((elem) => {
       if (typeof elem == "string") {
         return elem;
       } else {
         return mapToElements(elem);
       }
-    })
+    });
 
-    htmlmap.tag = htmlmap.tag == "body" ? "div" : htmlmap.tag
+    htmlmap.tag = htmlmap.tag == "body" ? "div" : htmlmap.tag;
 
-    const newElem = createElement(htmlmap.tag, htmlmap.attrs, ...contents)
+    const newElem = createElement(htmlmap.tag, htmlmap.attrs, ...contents);
     return newElem;
   } else {
-    const newElem = createElement(htmlmap.tag, htmlmap.attrs)
+    const newElem = createElement(htmlmap.tag, htmlmap.attrs);
     return newElem;
-  } 
-}
-   
-
-export async function getServerSideProps(context) {
-    const bookId = context.query.bookId;
-    const res = await fetch(`https://api.txtropy.com/books/${bookId}`);
-    const htmlmap = await res.json();
-    
-    return { props: { htmlmap } }
   }
+}
+
+export async function getStaticPaths() {
+  const res = await fetch("https://api.txtropy.com/books");
+  const jres = await res.json();
+  const books = await JSON.parse(jres.body);
+  const paths = books.map((book) => ({
+    params: { bookId: `${book.id}` },
+  }));
+
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`https://api.txtropy.com/books/${params.bookId}`);
+  const htmlmap = await res.json();
+
+  return { props: { htmlmap } };
+}
 
 function Page({ htmlmap }) {
-  const router = useRouter()
+  const router = useRouter();
   const bookId = router.query.bookId;
 
-  //         <Drawer />     
-  // console.log(htmlmap);
+  //         <Drawer />
   return (
-    <Container >
+    <Container>
       <Head>
-        <link rel="stylesheet" href={`https://api.txtropy.com/books/css/${bookId}`} type="text/css"/>
+        <link
+          rel="stylesheet"
+          href={`https://api.txtropy.com/books/css/${bookId}`}
+          type="text/css"
+        />
       </Head>
-        {mapToElements(htmlmap)}
+      {mapToElements(htmlmap)}
     </Container>
-
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
-import styles from '@/styles/page.module.css';
-
+import styles from "@/styles/page.module.css";
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch('https://api.txtropy.com/books')
-  const jres = await res.json()
-  const books = await JSON.parse(jres.body)
-  return { props: { books } }
+  const res = await fetch("https://api.txtropy.com/books");
+  const jres = await res.json();
+  const books = await JSON.parse(jres.body);
+  return { props: { books } };
 }
-
 
 export default function Page({ books }) {
   return (
@@ -25,27 +24,47 @@ export default function Page({ books }) {
       <CssBaseline />
       <main className={styles.layout}>
         <Box sx={{ paddingTop: 8 }}>
-          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Relative Entropy
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+          >
+            Textropy
           </Typography>
-          <Typography variant="h6" align="center" color="textSecondary" component="p">
-            Quickly build an effective pricing table for your potential customers with this layout.
-            It&apos;s built with default Material-UI components with little customization.
+          <Typography
+            variant="h6"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Textropy is a portmanteau of the words <i>Text</i> and{" "}
+            <i>Entropy</i>.{" "}
+            <a href="https://en.wikipedia.org/wiki/Jensen%E2%80%93Shannon_divergence">
+              Jensen-Shannon distance
+            </a>{" "}
+            has been used to find relations between the following books:
           </Typography>
         </Box>
 
         <Grid container spacing={4}>
-          {books.map(tier => (
+          {books.map((tier) => (
             <Grid item key={tier.id} xs={12} sm={6} md={4}>
               <Card>
-                <CardContent>
-                  <Typography><Box component="span" fontWeight='fontWeightBold'>{tier.title}</Box></Typography>
-                  {tier.authors.map(author => (
-                    <Typography key={`${tier.id}-${author}`} component="p">
-                      {author}
+                <CardActionArea href={`/books/${tier.id}`}>
+                  <CardContent>
+                    <Typography>
+                      <Box component="span" fontWeight="fontWeightBold">
+                        {tier.title}
+                      </Box>
                     </Typography>
-                  ))}
-                </CardContent>
+                    {tier.authors.map((author) => (
+                      <Typography key={`${tier.id}-${author}`} component="i">
+                        {author}
+                      </Typography>
+                    ))}
+                  </CardContent>
+                </CardActionArea>
               </Card>
             </Grid>
           ))}
